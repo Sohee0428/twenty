6743f243e4e04d09d74905eebbbf3d771624ce96
Sohee <sohee3480@gmail.com>
2021-04-21 19:40:23 +0900

    xml을 이용하지 않고 MainActivity.kt에서 버튼 만들기

--- app/src/main/java/com/example/twenty/MainActivity.kt
+++ app/src/main/java/com/example/twenty/MainActivity.kt
@@ -3,12 +3,27 @@ package com.example.twenty
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.Toast
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val linearLayout = LinearLayout(this)
+
+        val button1 = Button(this)
+        button1.text = "Button1"
+        linearLayout.addView(button1)
+
+        val button2 = Button(this)
+        button2.text = "Button2"
+        linearLayout.addView(button2)
+
+        setContentView(linearLayout)
 
 
     }
